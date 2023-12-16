0754f3d4675d43990c5bd20ac501e304847fc2ac
Akash <akashdeep.samantra@protonmail.com>
2023-12-16 12:16:22 +0530

    Sample web component

--- packages/core/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { LitElement, html } from 'lit';
-import { customElement } from 'lit/decorators.js';
-
-@customElement('my-element')
-export default class MyElement extends LitElement {
-    override render() {
-        return html`<p>Hello from my template.</p>`;
-    }
-}
